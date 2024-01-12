02429b9e84d11d83036c28568fe7b40a71aed879
Sebastian Wolnowski <73168676+SpaceAndFrogs@users.noreply.github.com>
2024-01-12 12:23:18 +0100

    Dodano zawartość funkcji w klasie Obstacle
    Dodano zawartość wszystkich funkcji w klasie Obstacle.
    Utworzono CubePrefab z klasą Obstacle i komponentem NavMeshObstacle, dodano kopie obiektu do obiektu ground i ustawiono dla nich odpopwiednie wartości.

--- Zadanie rekrutacyjne/Assets/Scripts/Obstacle.cs
+++ Zadanie rekrutacyjne/Assets/Scripts/Obstacle.cs
@@ -7,8 +7,6 @@ public class Obstacle : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
     [SerializeField]
-    private bool isMovingInZ;
-    [SerializeField]
     private bool isMovingInX;
     [SerializeField]
     private float moveMagnitude;
@@ -19,16 +17,52 @@ public class Obstacle : MonoBehaviour
     private Vector2 startXZ;
     private void Start()
     {
-        
+        startXZ = new Vector2(transform.position.x, transform.position.z);
     }
 
     private void FixedUpdate()
     {
-        
+        Move();
     }
 
     private void Move()
     {
-
+        if(isMovingInX) 
+        {
+            if(toMinusX) 
+            {
+                transform.position += Vector3.left * moveSpeed;
+                if(transform.position.x <= startXZ.x - moveMagnitude) 
+                {
+                    toMinusX = false;
+                }
+            }else
+            {
+                transform.position += Vector3.right * moveSpeed;
+                if (transform.position.x >= startXZ.x + moveMagnitude)
+                {
+                    toMinusX = true;
+                }
+            }
+        }
+        else
+        {
+            if (toMinusZ)
+            {
+                transform.position += Vector3.back * moveSpeed;
+                if (transform.position.z <= startXZ.y - moveMagnitude)
+                {
+                    toMinusZ = false;
+                }
+            }
+            else
+            {
+                transform.position += Vector3.forward * moveSpeed;
+                if (transform.position.z >= startXZ.y + moveMagnitude)
+                {
+                    toMinusZ = true;
+                }
+            }
+        }
     }
 }
